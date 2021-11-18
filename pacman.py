5ab4b09c7b429f9860818a380d567b378f91f472
Matteo Franchini <94017935+MatteoFranchini01@users.noreply.github.com>
2021-11-18 19:39:21 +0100

    Add files via upload

--- pacman.py
+++ pacman.py
@@ -2,9 +2,11 @@ import g2d
 from random import choice
 from pacman_map import in_wall
 
+
 class Actor():
     '''Interface to be implemented by each game character
     '''
+
     def move(self):
         '''Called by Arena, at the actor's turn
         '''
@@ -34,9 +36,11 @@ class Actor():
         '''
         raise NotImplementedError('Abstract method')
 
+
 class Arena():
     '''A generic 2D game, with a given size in pixels and a list of actors
     '''
+
     def __init__(self, size: (int, int)):
         '''Create an arena, with given dimensions in pixels
         '''
@@ -71,8 +75,8 @@ class Arena():
                     # reversed order, so actors drawn on top of others
                     # (towards the end of the cycle) are checked first
                     if other is not a and self.check_collision(a, other):
-                            a.collide(other)
-                            other.collide(a)
+                        a.collide(other)
+                        other.collide(a)
         self._count += 1
 
     def check_collision(self, a1: Actor, a2: Actor) -> bool:
@@ -82,8 +86,8 @@ class Arena():
         x1, y1, w1, h1 = a1.position() + a1.size()
         x2, y2, w2, h2 = a2.position() + a2.size()
         return (y2 < y1 + h1 and y1 < y2 + h2
-            and x2 < x1 + w1 and x1 < x2 + w2
-            and a1 in self._actors and a2 in self._actors)
+                and x2 < x1 + w1 and x1 < x2 + w2
+                and a1 in self._actors and a2 in self._actors)
 
     def actors(self) -> list:
         '''Return a copy of the list of actors
@@ -100,6 +104,7 @@ class Arena():
         '''
         return self._count
 
+
 class Biscuit():
     def size(self) -> (int, int):
         raise NotImplementedError('Abstract method')
@@ -110,11 +115,9 @@ class Biscuit():
     def position(self) -> (int, int):
         raise NotImplementedError('Abstract method')
 
-    def punteggio(self):
-        raise NotImplementedError('Abstract method')
-
     def collide(self, other: 'Actor'):
         raise NotImplementedError('Abstract method')
+
 
 class PacMan(Actor):
     def __init__(self, arena, pos, lives):
@@ -128,11 +131,10 @@ class PacMan(Actor):
         self._arena = arena
         self._symbol = (16, 0)
         arena.add(self)
-        self._power = False
 
     def move(self):
         arena_w, arena_h = self._arena.size()
-        if in_wall(self._x + self._dx, self._y + self._dy) == False:
+        if not in_wall(self._x + self._dx, self._y + self._dy):
             self._y += self._dy
             if self._y < 0:
                 self._y = 0
@@ -147,10 +149,10 @@ class PacMan(Actor):
 
     def control(self, keys):
         u, d, l, r = "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"
-        #u, d, l, r = "w", "s", "a", "d"
+        # u, d, l, r = "w", "s", "a", "d"
         dy_old, dx_old = self._dy, self._dx
 
-        if in_wall(self._x + self._dx, self._y + self._dy) == False:
+        if not in_wall(self._x + self._dx, self._y + self._dy):
             if self._x % 8 == 0 and self._y % 8 == 0:
                 if u in keys:
                     self._dy = -self._speed
@@ -168,7 +170,7 @@ class PacMan(Actor):
         else:
             self._dx, self._dy = 0, 0
 
-        if in_wall(self._x + self._dx, self._y + self._dy) == True:
+        if in_wall(self._x + self._dx, self._y + self._dy):
             if self._x % 8 == 0 and self._y % 8 == 0:
                 self._dy, self._dx = dy_old, dx_old
 
@@ -176,17 +178,13 @@ class PacMan(Actor):
         return self._lives
 
     def collide(self, other):
-        if self._arena.count() - self._last_collision < 30:
-            return
-        self._last_collision = self._arena.count()
-
         if isinstance(other, Ghost):
             if self._lives != 0:
                 self._lives -= 1
                 self._x, self._y = self._x_in, self._y_in
                 self._dx, self._dy = 0, 0
 
-        elif isinstance(other, Ghost_alt):
+        elif isinstance(other, GhostAlt):
             if self._lives != 0:
                 self._lives -= 1
                 self._x, self._y = self._x_in, self._y_in
@@ -213,18 +211,19 @@ class PacMan(Actor):
 
         return self._symbol
 
+
 class Ghost(Actor):
     def __init__(self, arena, pos, color):
         self._x, self._y = pos
         self._w, self._h = 16, 16
         self._dx, self._dy = 0, 2
         self._arena = arena
-        self._color = color #1 =< color =< 4
+        self._color = color  # 1 =< color =< 4
         arena.add(self)
 
     def move(self):
         val = [(0, 2), (0, -2), (2, 0), (-2, 0)]
-        if in_wall(self._x + self._dx, self._y + self._dy) == True:
+        if in_wall(self._x + self._dx, self._y + self._dy):
             if self._x % 8 == 0 and self._y % 8 == 0:
                 val.remove((self._dx, self._dy))
                 self._dx, self._dy = choice(val)
@@ -255,7 +254,8 @@ class Ghost(Actor):
         elif self._dy > 0:
             return 112, (64 + 16 * self._color)
 
-class Ghost_alt(Actor):
+
+class GhostAlt(Actor):
     def __init__(self, arena, pos):
         self._x, self._y = pos
         self._w, self._h = 16, 16
@@ -266,7 +266,7 @@ class Ghost_alt(Actor):
 
     def move(self):
         arena_w, arena_h = self._arena.size()
-        if in_wall(self._x + self._dx, self._y + self._dy) == False:
+        if not in_wall(self._x + self._dx, self._y + self._dy):
             self._y += self._dy
             if self._y < 0:
                 self._y = 0
@@ -285,7 +285,7 @@ class Ghost_alt(Actor):
         # u, d, l, r = "w", "s", "a", "d"
         dy_old, dx_old = self._dy, self._dx
 
-        if in_wall(self._x + self._dx, self._y + self._dy) == False:
+        if not in_wall(self._x + self._dx, self._y + self._dy):
             if self._x % 8 == 0 and self._y % 8 == 0:
                 if u in keys:
                     self._dy = -self._speed
@@ -303,7 +303,7 @@ class Ghost_alt(Actor):
         else:
             self._dx, self._dy = 0, 0
 
-        if in_wall(self._x + self._dx, self._y + self._dy) == True:
+        if in_wall(self._x + self._dx, self._y + self._dy):
             if self._x % 8 == 0 and self._y % 8 == 0:
                 self._dy, self._dx = dy_old, dx_old
 
@@ -328,6 +328,7 @@ class Ghost_alt(Actor):
         else:
             return 16, 64
 
+
 class BisPic(Biscuit):
     def __init__(self, arena, gioco, pos):
         self._arena = arena
@@ -344,9 +345,6 @@ class BisPic(Biscuit):
     def symbol(self):
         return 32 + 4, 88 + 4
 
-    def number(self):
-        return 32, 30
-
     def position(self):
         return (self._y - 2) * self._COST, self._x * self._COST
 
@@ -357,6 +355,7 @@ class BisPic(Biscuit):
 
     def move(self):
         pass
+
 
 class BisGr(Biscuit):
     def __init__(self, arena, gioco, pos):
@@ -372,7 +371,7 @@ class BisGr(Biscuit):
         return self._W, self._H
 
     def symbol(self):
-        return 176,48
+        return 176, 48
 
     def position(self):
         return (self._y - 2.7) * self._COST, (self._x - 1) * self._COST
@@ -385,11 +384,12 @@ class BisGr(Biscuit):
     def move(self):
         pass
 
+
 class PacManGame:
     def __init__(self, level: str, alternative: str):
         self._level = level
         self._alternative = alternative
-        self._arena = Arena((232, 280)) #Arena 232, 256. Arena con spazio 232, 280
+        self._arena = Arena((232, 280))  # Arena 232, 256. Arena con spazio 232, 280
         self._score = 0
         with open("Board.txt") as board:
             b = board.readlines()
@@ -420,7 +420,7 @@ class PacManGame:
 
         elif self._alternative == "2":
             self._hero = PacMan(self._arena, (8, 8), 3)
-            self._ghost_alt = Ghost_alt(self._arena, (88, 88))
+            self._ghost_alt = GhostAlt(self._arena, (88, 88))
 
     def arena(self) -> Arena:
         return self._arena
@@ -428,7 +428,7 @@ class PacManGame:
     def hero(self) -> PacMan:
         return self._hero
 
-    def ghost_alt(self) -> Ghost_alt:
+    def ghost_alt(self) -> GhostAlt:
         return self._ghost_alt
 
     def add_score(self, x: int):
@@ -445,12 +445,13 @@ class PacManGame:
         if self._score == 24800:
             return True
 
+
 class PacManGui:
     def __init__(self, boolean: bool, level: str, alternative: str):
         self._level, self._alternative = level, alternative
         self._bool = boolean
         self._welcome = self._bool
-        if self._welcome == False:
+        if not self._welcome:
             self._game = PacManGame(self._level, self._alternative)
             g2d.init_canvas(self._game.arena().size())
             self._sprites = g2d.load_image("pac-man.png")
@@ -459,30 +460,33 @@ class PacManGui:
             self.tick()
 
     def tick(self):
-        if self._welcome == True:
+        if self._welcome:
             self._alternative = g2d.prompt("Benvenuto, scegli il numero di giocatori (1 o 2): ")
             if self._alternative == "1":
-                self._level = g2d.prompt("Inserire il livello di difficoltà a cui si intende giocare.\nInserire E, M, H per scegliere il livello")
+                self._level = g2d.prompt(
+                    "Inserire il livello di difficoltà a cui si intende giocare.\nInserire E, M, H per scegliere il livello")
                 if self._level == "e":
                     g2d.alert("Hai a disposizione 3 vite e nella mappa compariranno 2 fantasmi")
                 elif self._level == "m":
                     g2d.alert("Hai a disposizione 3 vite e nella mappa compariranno 3 fantasmi")
                 elif self._level == "h":
-                    g2d.alert("Hai selezionato il livello più difficile, hai a disposizione 2 vite e compariranno 4 fantasmi")
+                    g2d.alert(
+                        "Hai selezionato il livello più difficile, hai a disposizione 2 vite e compariranno 4 fantasmi")
                 else:
                     g2d.alert("Valore Errato")
                     self.tick()
                 g2d.alert("Lo scopo del gioco è mangiare tutti i biscotti senza essere colpiti dai fantasmi")
             elif self._alternative == "2":
                 g2d.alert("PacMan viene controllato tramite le frecce\n\nIl fantasma tramite i tasti \nw, a, s, d")
-                g2d.alert("Il giocatore (PacMan) dovrà mangiare tutti i biscotti senza essere colpito dal giocatore (Fantasma)")
+                g2d.alert(
+                    "Il giocatore (PacMan) dovrà mangiare tutti i biscotti senza essere colpito dal giocatore (Fantasma)")
             else:
                 g2d.alert("Valore Errato")
                 self.tick()
             self._welcome = False
             PacManGui(self._welcome, self._level, self._alternative)
 
-        elif self._welcome == False:
+        elif not self._welcome:
             self._game.hero().control(g2d.current_keys())
             if self._alternative == "2":
                 self._game.ghost_alt().control(g2d.current_keys())
@@ -500,12 +504,11 @@ class PacManGui:
             g2d.draw_text_centered("Score: " + str(self._game.score()), (175, 265), 20)
 
             if self._game.game_over():
-                 g2d.alert("Game over")
-                 g2d.close_canvas()
+                g2d.alert("Game over")
+                g2d.close_canvas()
             elif self._game.game_won():
-                 g2d.alert("Game won")
-                 g2d.close_canvas()
+                g2d.alert("Game won")
+                g2d.close_canvas()
+
 
 PacManGui(True, "", "")
-
-
